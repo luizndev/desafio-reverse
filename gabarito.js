040a525a735052da30285d4761c6a7eae4a35ab8
Luis Eduardo Andrade <79378948+luizndev@users.noreply.github.com>
2025-06-23 14:54:30 -0300

    Update gabarito.js

--- gabarito.js
+++ gabarito.js
@@ -1,36 +1,36 @@
 
-// function reverseString(valor){
-//     let sign;
-//     let haveSign = false;
-//     if (valor.split('')[0] === '-') {
-//       sign = valor.split('')[0];
-//       haveSign = true;
-//     }
+function reverseString(valor){
+    let sign;
+    let haveSign = false;
+    if (valor.split('')[0] === '-') {
+      sign = valor.split('')[0];
+      haveSign = true;
+    }
     
-//     let totalWrite = valor.split('');
-//     let reverseString = [];
-//     switch (haveSign) {
-//       case true:
-//         for (let i = totalWrite.length - 1; i > 0; i--) {
-//           reverseString.push(totalWrite[i]);
-//         }
-//         break;
-//       default:
-//         for (let i = totalWrite.length - 1; i >= 0; i--) {
-//           reverseString.push(totalWrite[i]);
-//         }
-//         break;
-//     }
+    let totalWrite = valor.split('');
+    let reverseString = [];
+    switch (haveSign) {
+      case true:
+        for (let i = totalWrite.length - 1; i > 0; i--) {
+          reverseString.push(totalWrite[i]);
+        }
+        break;
+      default:
+        for (let i = totalWrite.length - 1; i >= 0; i--) {
+          reverseString.push(totalWrite[i]);
+        }
+        break;
+    }
 
-//     let response = ""
-//     reverseString.map(item => {
-//         response = response + item 
-//     })
-//     if(sign){
-//         console.log("-" + response)
-//     }else{
-//         console.log(response)
-//     }
-// }
+    let response = ""
+    reverseString.map(item => {
+        response = response + item 
+    })
+    if(sign){
+        document.getElementById("app").innerHTML = "-" + response
+    }else{
+        document.getElementById("app").innerHTML = response
+    }
+}
 
 // reverseString("-123")
